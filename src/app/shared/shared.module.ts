d8f967e127135e5912fa975c84017f58690ba1a1
rbuzzibarrios <rbuzzibarrios@gmail.com>
2019-10-13 04:28:56 -0400

    add mat-select-autocomplete component. Use to select destination selector. Add fontawesome style.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,39 @@ import {ModuleWithProviders, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SimpleSelectorComponent} from './simple-selector/simple-selector/simple-selector.component';
 import {HttpClientModule} from '@angular/common/http';
-import {MatSelectModule} from '@angular/material';
+import {
+    MatAutocompleteModule,
+    MatCheckboxModule,
+    MatFormFieldModule, MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatToolbarModule
+} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {SelectAutocompleteModule} from 'mat-select-autocomplete';
+
+const BASE_MODULE = [ReactiveFormsModule, FormsModule, MatCheckboxModule, SelectAutocompleteModule, MatIconModule];
 
 @NgModule({
     declarations: [SimpleSelectorComponent],
     imports: [
+        ...BASE_MODULE,
         CommonModule,
         HttpClientModule,
-        MatSelectModule
+        MatSelectModule,
+        MatToolbarModule,
+        MatAutocompleteModule,
+        MatFormFieldModule,
+        MatInputModule,
     ],
     exports: [
+        ...BASE_MODULE,
         SimpleSelectorComponent,
-        MatSelectModule
+        MatSelectModule,
+        MatToolbarModule,
+        MatAutocompleteModule,
+        MatFormFieldModule,
+        MatInputModule,
     ],
 })
 
--- src/app/ui/components/booking/booking.component.ts
+++ src/app/ui/components/booking/booking.component.ts
@@ -1,7 +1,13 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
 import {SimpleSelectorSettings} from '../../../shared/simple-selector/simple-selector/simple-selector.component';
 import {FlightTypeService} from '../../../services/flight-type/flight-type.service';
 import {TravelClassService} from '../../../services/travel-class/travel-class.service';
+import {Observable} from 'rxjs';
+import {FormControl} from '@angular/forms';
+import {map} from 'rxjs/operators';
+import {isArray} from 'util';
+
+declare const $: any;
 
 @Component({
     selector: 'app-booking',
@@ -26,11 +32,46 @@ export class BookingComponent implements OnInit {
         service: this.travelClassService,
     } as SimpleSelectorSettings;
 
+    myControl = new FormControl();
+    origins = [{id: 0, name: ''}];
+
     constructor(protected flightTypeService: FlightTypeService,
                 protected travelClassService: TravelClassService) {
+
+    }
+
+    filterOrigin(): Observable<any> {
+        return this.flightTypeService.getData()
+            .pipe(
+                map(({data}) => {
+                    const filterLength = String(this.myControl.value).length;
+                    if (filterLength <= 1) {
+                        data = [{id: 0, name: ''}];
+                    }
+                    if (isArray(data)) {
+                        return (data as Array<any>).filter((option) => {
+                            return String(option.name.toLowerCase()).startsWith(this.myControl.value.toLowerCase());
+                        });
+                    }
+                })
+            );
     }
 
     ngOnInit() {
-    }
+        $('body')
+            .on('keyup', '#cdk-overlay-0 input[type=text]', () => {
+                console.log('change');
+                const filterInput = $('#cdk-overlay-0 input[type=text]').val();
+                this.myControl.setValue(filterInput, {emitEvent: true});
+            }).bind(this);
 
+        this.myControl.valueChanges
+            .subscribe((value) => {
+                if (String(value).length >= 2) {
+                    this.filterOrigin().subscribe((data) => {
+                        this.origins = data;
+                    });
+                }
+            });
+    }
 }
